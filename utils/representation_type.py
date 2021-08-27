8b8ae7139332020412dd08cad56b852b974f73c1
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-08-26 22:32:17 -0300

    remove files

--- utils/representation_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class Representation(Enum):
-    ADJACENCY_LIST = 0
-    ADJACENCY_MATRIX = 1
--- utils/search_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class Search_type(Enum):
-    DEEP_SEARCH = 0
-    WIDE_SEARCH = 1
